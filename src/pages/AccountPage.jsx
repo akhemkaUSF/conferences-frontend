adfb52b5cf1b96c317d2f3b7d7b0114979f3bc79
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-20 11:55:03 -0400

    Account Page configuration

---
+++ src/pages/AccountPage.jsx
@@ -0,0 +1,40 @@
+import {Link, useLocation} from "react-router-dom";
+
+export default function AccountNav() {
+  const {pathname} = useLocation();
+  let subpage = pathname.split('/')?.[2];
+  if (subpage === undefined) {
+    subpage = 'profile';
+  }
+  function linkClasses (type=null) {
+    let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
+    if (type === subpage) {
+      classes += ' bg-primary text-white';
+    } else {
+      classes += ' bg-gray-200';
+    }
+    return classes;
+  }
+  return (
+    <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+      <Link className={linkClasses('profile')} to={'/account'}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+        My profile
+      </Link>
+      <Link className={linkClasses('bookings')} to={'/account/bookings'}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+        My bookings
+      </Link>
+      <Link className={linkClasses('places')} to={'/account/places'}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+        My accommodations
+      </Link>
+    </nav>
+  );
+}
