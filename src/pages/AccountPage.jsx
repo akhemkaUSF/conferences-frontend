27b58780376959fb28fa41fed1dfac0f469cf0f1
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-20 20:08:17 -0400

    changing background color

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -9,7 +9,7 @@ export default function AccountPage() {
   function linkClasses (type=null) {
     let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
     if (type === subpage) {
-      classes += ' bg-pink text-white';
+      classes += ' background-color:#F5385D text-white';
     } else {
       classes += ' bg-gray-200';
     }
