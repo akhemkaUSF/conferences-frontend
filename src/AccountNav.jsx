5f99602192385176e8a9c6e0fb396cd113302583
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-22 19:01:13 -0400

    corrected the header for profile in the account nav bar

--- src/AccountNav.jsx
+++ src/AccountNav.jsx
@@ -2,15 +2,19 @@ import {Link, useLocation} from "react-router-dom";
 
 export default function AccountNav() {
   const {pathname} = useLocation();
+  //uses the path to determine whether we are currently on the profile, bookings, or places page
   let subpage = pathname.split('/')?.[2];
   if (subpage === undefined) {
     subpage = 'profile';
   }
+  //types is passed with the function, as either 'profile', 'bookings', or 'places'
   function linkClasses (type=null) {
     console.log("I don't understand, is this thing even running?");
     let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
+    //the header for the subpage we are on is pink
     if (type === subpage) {
       classes += ' bg-pink-600 text-white';
+    //the header for the two other pages are gray
     } else {
       classes += ' bg-gray-200';
     }
@@ -22,7 +26,7 @@ export default function AccountNav() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
         </svg>
-        My prolific
+        My profile
       </Link>
       <Link className={linkClasses('bookings')} to={'/account/bookings'}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
