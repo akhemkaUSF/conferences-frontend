8cebc93a823b6b7b8b078cd9966c1bfe7b51f8aa
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-22 18:38:15 -0400

    test change to determine reason for existent of subpage

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -23,25 +23,27 @@ export default function ProfilePage() {
     return 'Loading...';
   }
 
+  //go to login if our user doesn't exist and the page has already loaded
   if (ready && !user && !redirect) {
     return <Navigate to={'/login'} />
   }
 
+  //instead of setting redirect to a boolean, it sets redirect to a string, and sends our website there
   if (redirect) {
     return <Navigate to={redirect} />
   }
   return (
     <div>
       <AccountNav />
-      {subpage === 'profile' && (
+      {/*{subpage === 'profile' && (*/}
         <div className="text-center max-w-lg mx-auto">
           Logged in as {user.name} ({user.email})<br />
           <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
         </div>
-      )}
-      {subpage === 'places' && (
+      {/*)*/}
+      {/*{subpage === 'places' && (
         <PlacesPage />
-      )}
+      )}*/}
     </div>
   );
 }
