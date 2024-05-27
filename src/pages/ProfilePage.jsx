9e3ca081a925d166816d023bb980709174394336
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-27 18:04:23 -0400

    Addressed import issue on ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,7 +2,7 @@ import {useContext, useState} from "react";
 import {UserContext} from "../UserContext.jsx";
 import {Link, Navigate, useParams} from "react-router-dom";
 import axios from "axios";
-import PlacesPage from "./PlacesPage";
+import ConferencesPage from "./ConferencesPage";
 import AccountNav from "../AccountNav";
 
 export default function ProfilePage() {
@@ -41,8 +41,8 @@ export default function ProfilePage() {
           <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
         </div>
       )}
-      {subpage === 'places' && (
-        <PlacesPage />
+      {subpage === 'conferences' && (
+        <ConferencesPage />
       )}
     </div>
   );
