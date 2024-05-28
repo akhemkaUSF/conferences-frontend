90e93a7c1d7e63a75ef50bc7ad82e280ec218a64
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-27 20:11:21 -0400

    corrected variable issue on Indexpage.jsx

--- src/pages/Indexpage.jsx
+++ src/pages/Indexpage.jsx
@@ -15,9 +15,9 @@ export default function IndexPage() {
     //creates it as a grid 
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
       {/*for each value in the conferences array do the following*/}
-      {conferences.length > 0 && conferences.map(place => (
+      {conferences.length > 0 && conferences.map(conference => (
         //{/*links to the individual conferences page using the id*/}
-        <Link to={'/place/'+place._id}>
+        <Link to={'/conference/'+conference._id}>
           <div className="bg-gray-500 mb-2 rounded-2xl flex">
             {/*displays the first photo in the conferences array of photos
             Question mark ensures that if photos does not exist or is null or undefined, the code does not throw an error 
