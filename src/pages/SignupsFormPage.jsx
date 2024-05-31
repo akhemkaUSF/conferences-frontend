4479f8badfb7b40e48c3fbc34f7cd9c002d9af34
Anuj Khemka <anuj.khemka.us@gmail.com>
2024-05-31 10:01:39 -0400

    added additional console.log statement

--- src/pages/SignupsFormPage.jsx
+++ src/pages/SignupsFormPage.jsx
@@ -48,7 +48,7 @@ export default function SignupsFormPage() {
   //activated when we submit the places form
   async function saveSignup(ev) {
     const data = await axios.get('/profile');
-    console.log(data);
+    console.log("what is happening here" + data);
     if (data==null) {
       alert('please log in first');
       return;
